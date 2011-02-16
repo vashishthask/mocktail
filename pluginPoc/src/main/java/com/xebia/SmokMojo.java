fb6e07006bd7f558a73afec3d8a6fba703029168
Ganesh Gembali <ggembali@xebia.com>
2011-02-16 17:42:59 +0530

    Initial projects for maven plugin
    Former-commit-id: 6bda885585ed1d34f2d79210c117a6c1e1a2bf9a

---
+++ pluginPoc/src/main/java/com/xebia/SmokMojo.java
@@ -0,0 +1,102 @@
+package com.xebia;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+
+/**
+ * Goal which touches a timestamp file.
+ * 
+ * @goal touch
+ * 
+ * @phase process-sources
+ */
+public class SmokMojo extends AbstractMojo {
+    /**
+     * @parameter expression=”${smok.recording}”
+     * @required
+     */
+    private File recordingDirectory;
+
+    /**
+     * List of Resource objects for the current build, containing directory,
+     * includes, and excludes.
+     * @parameter default-value="${project.resources}"
+     * @required
+     * @readonly
+     */
+    private List resources;
+
+    public void execute() throws MojoExecutionException {
+        if (resources != null && !resources.isEmpty()) {
+            for (Iterator it = resources.iterator(); it.hasNext();) {
+                Resource resource = (Resource) it.next();
+                String resourceRoot = resource.getDirectory();
+                System.out.println(resourceRoot);
+            }
+        }
+        
+        File f = recordingDirectory;
+        System.out.println(recordingDirectory);
+        if (!f.exists()) {
+            System.out.println(recordingDirectory);
+            f.mkdirs();
+        }
+
+        File touch = new File(f, "touch.txt");
+
+        FileWriter w = null;
+        try {
+            w = new FileWriter(touch);
+
+            w.write("touch.txt");
+        }
+        catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        }
+        catch (Exception e) {
+            throw new MojoExecutionException("Damn it", e);
+        }
+        finally {
+            if (w != null) {
+                try {
+                    w.close();
+                }
+                catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    public File getRecordingDirectory() {
+        return recordingDirectory;
+    }
+
+    public void setRecordingDirectory(File recordingDirectory) {
+        this.recordingDirectory = recordingDirectory;
+    }
+}
