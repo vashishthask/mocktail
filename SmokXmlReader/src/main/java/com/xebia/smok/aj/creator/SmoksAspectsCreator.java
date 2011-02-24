6d8653833be3185bf9f3638a325c1f3d487e03ba
Sandeep Rawat <sandeep_r80@yahoo.co.in>
2011-02-24 18:45:09 +0530

    first version of changes are done
    Former-commit-id: 7abd7fa08803c5ad2ce9b42dd4e82602f477d34e

--- SmokXmlReader/src/main/java/com/xebia/smok/aj/creator/SmoksAspectsCreator.java
+++ SmokXmlReader/src/main/java/com/xebia/smok/aj/creator/SmoksAspectsCreator.java
@@ -1,5 +1,6 @@
 package com.xebia.smok.aj.creator;
 
+import java.io.File;
 import java.util.List;
 
 import com.xebia.smok.xml.domain.Smok;
@@ -7,13 +8,13 @@ import com.xebia.smok.xml.domain.Smok;
 public enum SmoksAspectsCreator {
 	ASPECTS_CREATOR;
 	
-	public int createAspects(List<Smok> smoksForAspects) {
+	public int createAspects(List<Smok> smoksForAspects, File aspectsDirectory) {
 		int aspectsCreated = 0;
 		AspectCreator<Smok> smokAspectCreator;
 		for (Smok smok : smoksForAspects) {
 			smokAspectCreator = SmokAspectCreatorFactory.getAspectCreator(smok.onlyForClass());
 			try {
-				smokAspectCreator.createAspect(smok);
+				smokAspectCreator.createAspect(smok, aspectsDirectory);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
