6dc4631149cdfad01b2861496fbbd05cd0634b2c
Sandeep Rawat <sandeep_r80@yahoo.co.in>
2011-03-17 17:24:38 +0530

    created a utility class to be used in testing
    Former-commit-id: 716167911f194972a430626e539f0256a01e0d80

--- SmokXmlReader/src/test/java/com/xebia/smok/aj/creator/SmoksAspectsCreatorTest.java
+++ SmokXmlReader/src/test/java/com/xebia/smok/aj/creator/SmoksAspectsCreatorTest.java
@@ -1,9 +1,10 @@
 package com.xebia.smok.aj.creator;
 
+import static com.xebia.FileTestUtil.absolutePath;
+import static com.xebia.FileTestUtil.deleteDir;
 import static junit.framework.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.junit.Test;
 
@@ -16,44 +17,23 @@ public class SmoksAspectsCreatorTest {
 	@Test
 	public void testCreateAspects() throws Exception {
 		SmokContext.getSmokContext("recording_base_directory");
-		String testAspectsDirectory = getAbsolutePath("src", "test",
+		String testAspectsDirectory = absolutePath("src", "test",
 				"resources", "com", "xebia", "smok", "aspectsDirectory");
 		SmoksAspectsCreator.ASPECTS_CREATOR.createAspects(SmokObjectMother
 				.getSmoksForAspects(), new File(testAspectsDirectory),
 				SmokMode.RECORDING_MODE);
-		String firstSmokFile = getAbsolutePath("src", "test",
-				"resources", "com", "xebia", "smok", "aspectsDirectory","com","xebia","smok","aj","creator","AspectTemplateProcesser.aj");
+		String firstSmokFile = absolutePath("src", "test", "resources",
+				"com", "xebia", "smok", "aspectsDirectory", "com", "xebia",
+				"smok", "aj", "creator", "AspectTemplateProcesser.aj");
 		assertTrue(new File(firstSmokFile).isFile());
 
-		String secondSmokFile = getAbsolutePath("src", "test",
-				"resources", "com", "xebia", "smok", "aspectsDirectory","com","xebia","smok","xml","domain","AspectSmok.aj");
+		String secondSmokFile = absolutePath("src", "test", "resources",
+				"com", "xebia", "smok", "aspectsDirectory", "com", "xebia",
+				"smok", "xml", "domain", "AspectSmok.aj");
 		assertTrue(new File(secondSmokFile).isFile());
-		//Uncomment the deleteDir if you want to see the aspects
+		// Uncomment the deleteDir if you want to see the aspects
 		deleteDir(new File(testAspectsDirectory));
 	}
 
-	private boolean deleteDir(File dir) {
-		if (dir.isDirectory()) {
-	        String[] children = dir.list();
-	        for (int i=0; i<children.length; i++) {
-	            boolean success = deleteDir(new File(dir, children[i]));
-	            if (!success) {
-	                return false;
-	            }
-	        }
-	    }
-
-	    // The directory is now empty so delete it
-	    return dir.delete();
-
-	}
-
-	private String getAbsolutePath(String... path) throws IOException {
-		File file = new File(".");
-		StringBuffer absolutePath = new StringBuffer(file.getCanonicalPath());
-		for (String pathElement : path) {
-			absolutePath.append(File.separator).append(pathElement);
-		}
-		return absolutePath.toString();
-	}
+	
 }
