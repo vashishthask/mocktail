e955c610b63b733bb511fbbfcf67b7228af4fc97
Sandeep Rawat <sandeep_r80@yahoo.co.in>
2011-03-17 17:15:44 +0530

    Updated test case to verify aspects file creation
    Former-commit-id: 90b1181404dd8b53d621bcdd4939e452579bb1aa

--- SmokXmlReader/src/test/java/com/xebia/smok/aj/creator/SmoksAspectsCreatorTest.java
+++ SmokXmlReader/src/test/java/com/xebia/smok/aj/creator/SmoksAspectsCreatorTest.java
@@ -1,5 +1,7 @@
 package com.xebia.smok.aj.creator;
 
+import static junit.framework.Assert.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -19,6 +21,31 @@ public class SmoksAspectsCreatorTest {
 		SmoksAspectsCreator.ASPECTS_CREATOR.createAspects(SmokObjectMother
 				.getSmoksForAspects(), new File(testAspectsDirectory),
 				SmokMode.RECORDING_MODE);
+		String firstSmokFile = getAbsolutePath("src", "test",
+				"resources", "com", "xebia", "smok", "aspectsDirectory","com","xebia","smok","aj","creator","AspectTemplateProcesser.aj");
+		assertTrue(new File(firstSmokFile).isFile());
+
+		String secondSmokFile = getAbsolutePath("src", "test",
+				"resources", "com", "xebia", "smok", "aspectsDirectory","com","xebia","smok","xml","domain","AspectSmok.aj");
+		assertTrue(new File(secondSmokFile).isFile());
+		//Uncomment the deleteDir if you want to see the aspects
+		deleteDir(new File(testAspectsDirectory));
+	}
+
+	private boolean deleteDir(File dir) {
+		if (dir.isDirectory()) {
+	        String[] children = dir.list();
+	        for (int i=0; i<children.length; i++) {
+	            boolean success = deleteDir(new File(dir, children[i]));
+	            if (!success) {
+	                return false;
+	            }
+	        }
+	    }
+
+	    // The directory is now empty so delete it
+	    return dir.delete();
+
 	}
 
 	private String getAbsolutePath(String... path) throws IOException {
