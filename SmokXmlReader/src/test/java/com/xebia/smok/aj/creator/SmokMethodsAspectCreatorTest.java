ce09e24b19d44f933e0fc34469b8d1439a622d7f
Sandeep Rawat <sandeep_r80@yahoo.co.in>
2011-07-12 18:04:30 +0530

    Created metod template for playback aspect
    Former-commit-id: a00293eb530faf5a9678e042e60ecda7d90d7fe8

--- SmokXmlReader/src/test/java/com/xebia/smok/aj/creator/SmokMethodsAspectCreatorTest.java
+++ SmokXmlReader/src/test/java/com/xebia/smok/aj/creator/SmokMethodsAspectCreatorTest.java
@@ -44,21 +44,32 @@ public class SmokMethodsAspectCreatorTest {
 		}.createAspect(methodSmok, aspectDir);
 	}
 
-	/*@Test
+	@Test
 	public void shouldCreatePlaybackMethodAspects() throws Exception {
-		final Smok methodSmok = SmokObjectMother.createMethodSmok("FQCN2",
+		final Smok methodSmok = SmokObjectMother.createMethodSmok("FQCN2","com.xebia",
 				"method1", "method2");
+		SmokContainer.initializeContainer("");
+		SmokContext smokContext = SmokContext.getSmokContext("root_dir");
+		DirectFieldAccessor dfa = new DirectFieldAccessor(smokContext);
+		//Need to set as Smok Context is a singleton class and is getting set-upped from multiple places
+		dfa.setPropertyValue("recordingDirectory", "root_dir");
+		
 		new SmokMethodsAspectCreator(SmokMode.PLAYBACK_MODE) {
 			@Override
-			protected void createAspectFile(String fileName, File directory,
+			protected void createAspectFile(Smok smok, String fileName, File directory,
 					String templatedMethodObjectString) throws IOException {
+				assertThat(fileName, is(methodSmok.getClassName()));
+				assertThat(templatedMethodObjectString, containsString("recordingDirectoryPath = \"root_dir\";"));
+				assertThat(templatedMethodObjectString, containsString("String fqcn = \"com.xebia.FQCN2\";"));
+				assertThat(templatedMethodObjectString, containsString("pointcut callPointcutmethod1() : call(* com.xebia.FQCN2.method1(..));"));
+				assertThat(templatedMethodObjectString, containsString("pointcut callPointcutmethod2() : call(* com.xebia.FQCN2.method2(..));"));
 				assertThat(fileName, is(methodSmok.getClassName()));
 				assertThat(templatedMethodObjectString, containsString("method1"));
 				assertThat(templatedMethodObjectString, containsString("method2"));
 				assertThat(templatedMethodObjectString, containsString("around"));
-				assertThat(templatedMethodObjectString, containsString("I'll skip the actual execution"));
+				assertThat(templatedMethodObjectString, containsString("I'll represent the playback aspect"));
 			}
 		}.createAspect(methodSmok, aspectDir);
-	}*/
+	}
 
 }
