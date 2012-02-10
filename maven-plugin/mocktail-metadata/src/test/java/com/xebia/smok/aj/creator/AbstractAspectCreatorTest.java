6128899230416195d3564a7feadc9f4d54d4d807
Sandeep Rawat <sandeep@treechimp.com>
2012-02-10 14:21:14 +0530

    Updated test case to use JUnit Rule feature to have temporary folder
    Former-commit-id: 21cebfafd54ceae0ad283f14df8743ee9f126d3a

--- maven-plugin/mocktail-metadata/src/test/java/com/xebia/smok/aj/creator/AbstractAspectCreatorTest.java
+++ maven-plugin/mocktail-metadata/src/test/java/com/xebia/smok/aj/creator/AbstractAspectCreatorTest.java
@@ -2,8 +2,11 @@ package com.xebia.smok.aj.creator;
 
 import java.io.File;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import com.xebia.smok.SmokContainer;
 import com.xebia.smok.SmokObjectMother;
 import com.xebia.smok.xml.domain.AspectType;
 import com.xebia.smok.xml.domain.Smok;
@@ -12,12 +15,15 @@ import com.xebia.smok.xml.domain.SmokMode;
 
 public class AbstractAspectCreatorTest {
 
+	@Rule
+    public TemporaryFolder temporaryAspectDir= new TemporaryFolder();
 	
 	@Test
 	public void testAspectFileCreation() throws Exception {
-		AbstractAspectCreator<Smok> abstractAspectCreator = new AbstractSmokAspectCreator(AspectType.CLASS_ASPECT_TYPE, SmokMode.RECORDING_MODE) {
-		};
+		SmokContainer.initializeContainer("");
+		AbstractAspectCreator<Smok> abstractAspectCreator = new AbstractSmokAspectCreator(AspectType.CLASS_ASPECT_TYPE, SmokMode.RECORDING_MODE) {};
 		
-		abstractAspectCreator.createAspect(SmokObjectMother.createClassSmok("AspectedClass", ""), new File("C:/sandy/recording"));
+		File aspectsRootDirectory = temporaryAspectDir.newFolder("aspect");
+		abstractAspectCreator.createAspect(SmokObjectMother.createClassSmok("AspectedClass", ""), aspectsRootDirectory);
 	}
 }
