4ef1bdb01ebb6d5290f41ccdc6d9d86a56a90122
Sandeep Rawat <srawat@xebia.com>
2012-08-17 14:51:25 +0530

    Added a selenium test for vets list
    Former-commit-id: 15e23c089f65abe9af69e37d61818e25b8500311

---
+++ plugin-samples/spring-petclinic/src/test/java/org/springframework/samples/petclinic/web/selenium/VetsListTest.java
@@ -0,0 +1,59 @@
+package org.springframework.samples.petclinic.web.selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.Assert.*;
+
+public class VetsListTest {
+
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://localhost:8080/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testUntitled() throws Exception {
+		driver.get(baseUrl + "/petclinic/vets");
+		assertEquals(
+				"Henry Stevens",
+				driver.findElement(
+						By.xpath("//div[@id='main']/table/tbody/tr[6]/td"))
+						.getText());
+		assertEquals(
+				"radiology",
+				driver.findElement(
+						By.xpath("//div[@id='main']/table/tbody/tr[6]/td[2]"))
+						.getText());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+}
